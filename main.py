47c73db17087beeaebeac73d7ea8343fe18bcce1
Superbjorn <shindara09@gmail.com>
2023-04-22 10:06:57 +0200

    enable scheduler

--- main.py
+++ main.py
@@ -55,6 +55,8 @@ async def main():
 
     # run scheduler
     stop_run_continously = run_continously()
+    jobs = HonfiguratorSchedule(global_config)
+    jobs.setup_tasks()
 
     host = "127.0.0.1"
     game_server_to_mgr_port = global_config['hon_data']['svr_managerPort']
