0869727e1e57585ded8323d8cc5145037d2c8601
frankthetank001 <82205454+frankthetank001@users.noreply.github.com>
2023-07-24 00:06:44 +1000

    Update game_server_manager.py

--- cogs/game/game_server_manager.py
+++ cogs/game/game_server_manager.py
@@ -340,11 +340,11 @@ class GameServerManager:
         """
         mserver_auth_response = await self.master_server_handler.send_replay_auth(f"{self.global_config['hon_data']['svr_login']}:", hashlib.md5(self.global_config['hon_data']['svr_password'].encode()).hexdigest())
         if mserver_auth_response[1] != 200:
-            LOGGER.error(f"[{mserver_auth_response[1]}] Authentication to MasterServer failed.")
+            prefix = (f"[{mserver_auth_response[1]}] Authentication to MasterServer failed. ")
             if mserver_auth_response[1] in [401, 403]:
-                LOGGER.error(f"Please ensure your username and password are correct in {HOME_PATH / 'config' / 'config.json'}")
+                LOGGER.error(f"{prefix}Please ensure your username and password are correct in {HOME_PATH / 'config' / 'config.json'}")
             elif mserver_auth_response[1] > 500 and mserver_auth_response[1] < 600:
-                LOGGER.error(f"The issue is most likely server side.")
+                LOGGER.error(f"{prefix}The issue is most likely server side.")
             raise HoNAuthenticationError(f"[{mserver_auth_response[1]}] Authentication error.")
         LOGGER.interest("Authenticated to MasterServer.")
         parsed_mserver_auth_response = phpserialize.loads(mserver_auth_response[0].encode('utf-8'))
