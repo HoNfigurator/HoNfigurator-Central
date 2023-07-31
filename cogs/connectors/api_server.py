bbcc3dba9af0fd5bbf2412d60b4072c685d374d5
frankthetank001 <82205454+frankthetank001@users.noreply.github.com>
2023-07-31 23:55:07 +1000

    Update api_server.py

--- cogs/connectors/api_server.py
+++ cogs/connectors/api_server.py
@@ -737,9 +737,9 @@ async def start_server(port: str, token_and_user_info: dict = Depends(check_perm
         if len(results) > 1:
             return JSONResponse(status_code=200, content=results[1])
         else:
-            return JSONResponse(status_code=200)
+            return JSONResponse(status_code=200, content="OK")
     else:
-        return JSONResponse(status_code=500)
+        return JSONResponse(status_code=500, content="OK")
 
 @app.post("/api/add_servers/{num}", description="Add X number of game servers. Dynamically creates additional servers based on total allowed count.")
 async def add_all_servers(num: int, token_and_user_info: dict = Depends(check_permission_factory(required_permission="configure"))):
