ae6d544ab190bd61ccc021bba2119e4a3e41d21a
frankthetank001 <82205454+frankthetank001@users.noreply.github.com>
2023-08-09 00:32:24 +1000

    Added github branch to filebeat server field

--- utilities/filebeat.py
+++ utilities/filebeat.py
@@ -393,7 +393,8 @@ async def configure_filebeat(silent=False,test=False):
             'RAM': global_config['system_data']['total_ram'],
             'Priority': global_config['hon_data']['svr_priority'],
             'Affinity_Override': global_config['hon_data']['svr_override_affinity'] if operating_system == 'Windows' else None,
-            'BotMatch_Allowed': global_config['hon_data']['svr_enableBotMatch']
+            'BotMatch_Allowed': global_config['hon_data']['svr_enableBotMatch'],
+            'GitHub_Branch': MISC.get_current_github_branch()
         }
         filebeat_inputs = {}
         filebeat_inputs['slave_logs'] = \
