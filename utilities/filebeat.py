4f0bb35a5e3e0615919fdedd94e0956acb8a771d
frankthetank001 <82205454+frankthetank001@users.noreply.github.com>
2023-08-09 00:34:50 +1000

    resolve typo in getting github branch

--- utilities/filebeat.py
+++ utilities/filebeat.py
@@ -394,7 +394,7 @@ async def configure_filebeat(silent=False,test=False):
             'Priority': global_config['hon_data']['svr_priority'],
             'Affinity_Override': global_config['hon_data']['svr_override_affinity'] if operating_system == 'Windows' else None,
             'BotMatch_Allowed': global_config['hon_data']['svr_enableBotMatch'],
-            'GitHub_Branch': MISC.get_current_github_branch()
+            'GitHub_Branch': MISC.github_branch
         }
         filebeat_inputs = {}
         filebeat_inputs['slave_logs'] = \
