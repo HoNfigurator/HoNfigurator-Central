00e33f01f1ee024134cbc0dad880b305ff1d27d3
frankthetank001 <82205454+frankthetank001@users.noreply.github.com>
2023-08-22 02:13:12 +1000

    Update api_server.py

--- cogs/connectors/api_server.py
+++ cogs/connectors/api_server.py
@@ -246,7 +246,7 @@ async def set_hon_data(hon_data: dict = Body(...), token_and_user_info: dict = D
             global_config['hon_data'] = hon_data
             await manager_event_bus.emit('update_server_start_semaphore')
             await manager_event_bus.emit('config_change_hook_actions')
-            await manager_event_bus.emit('check_for_restart_required')
+            await manager_event_bus.emit('check_for_restart_required', disable=True)
     except ValueError as e:
         return JSONResponse(status_code=501, content=str(e))
 
