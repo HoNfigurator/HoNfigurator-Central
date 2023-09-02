da93c77e671ba7741a3cfd5c476e3df3c0596c19
frankthetank001 <82205454+frankthetank001@users.noreply.github.com>
2023-09-03 04:16:03 +1000

    Update proxy_http.py

--- cogs/TCP/proxy_http.py
+++ cogs/TCP/proxy_http.py
@@ -58,10 +58,10 @@ async def forward_data(src_reader, dst_writer, parser=None):
             if parser:
                 parser(data)
 
-            # new_user_agent = b"S2 Games/Heroes of Newerth/4.10.8.0/las/x86-biarch"
-            # data = data.replace(b"User-Agent: S2 Games/Heroes of Newerth/4.10.9.0/las/x86-biarch", b"User-Agent: " + new_user_agent)
+            new_user_agent = b"S2 Games/Heroes of Newerth/4.10.8.0/las/x86-biarch"
+            data = data.replace(b"User-Agent: S2 Games/Heroes of Newerth/4.10.9.0/las/x86-biarch", b"User-Agent: " + new_user_agent)
 
-            # my_print("Modified request:", data)
+            my_print("Modified request:", data)
 
             dst_writer.write(data)
             await dst_writer.drain()
