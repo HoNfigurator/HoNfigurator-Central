62479d9ca3f7fa825532b391c6d577f891b3938d
frankthetank001 <82205454+frankthetank001@users.noreply.github.com>
2023-10-01 23:58:50 +1100

    Update mqtt.py

--- cogs/handlers/mqtt.py
+++ cogs/handlers/mqtt.py
@@ -24,7 +24,7 @@ class MQTTHandler:
         # Create a new MQTT client instance
         self.client = mqtt.Client()
 
-        if get_misc().os_platform == "windows":
+        if get_misc().get_os_platform() == "windows":
             step_ca_dir = Path(os.environ["HOMEPATH"]) / ".step" / "certs"
         else:
             step_ca_dir = Path(os.environ["HOME"]) / ".step" / "certs"
